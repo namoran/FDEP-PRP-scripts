2f704574d1e005a7e0ab132233ab195597204ad0
namoran <namoran@gmail.com>
2021-03-07 01:19:50 -0500

    creating a separate file of functions to use in program or to use separately as needed

---
+++ EDD_functions.py
@@ -0,0 +1,21 @@
+def check_detection(qualifier):
+    '''checks to see if the analyte was detected by seeing if the qualifier is U'''
+        if qualifer = 'U':
+                return True
+        else:
+                return False
+
+def convert_units(result, units, new_units):
+    '''convert units of the results fed into the fuction'''
+    #the units tyoes are limited to ug/l ug/kg mg/kg mg/l ppm ppb
+    return converted_result, new_units
+
+def check_GCTL_exceedance(parameter, result, units, matrix, qualifier):
+    if matrix != 'W':
+        return None
+        pass
+    
+def checkSGCTL_exceedance(parameter, result, units, matrix, qualifier):
+    if matrix != 'S':
+        return None
+        pass
