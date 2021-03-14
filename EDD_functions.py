a4f38ca70f62e21b403f4f774f84690c03328cf1
nicholas <namoran@gmail.com>
2021-03-14 18:13:08 -0400

    completed crude unit converter

--- EDD_functions.py
+++ EDD_functions.py
@@ -1,16 +1,23 @@
 def check_detection(qualifier):
     '''checks to see if the analyte was detected by seeing if the qualifier is U'''
-        if qualifer = 'U':
-                return True
-        else:
-                return False
+    if qualifier == 'U':
+        return True
+    else:
+        return False
 
 def convert_units(result, units, new_units):
-    '''convert units of the results fed into the fuction'''
+    '''convert units of the results fed into the function'''
     #the units tyoes are limited to ug/l ug/kg mg/kg mg/l ppm ppb
-    if units == new_units:
-        return result, units
-    return converted_result, new_units
+    convert_sequence = {"ug/l":	 10**(-6),
+			"ug/kg": 10**(-6),
+			"ppb":	 10**(-6),
+			"mg/l":  10**(-3),
+			"mg/kg": 10**(-3),
+			"ppm":	 10**(-3),
+			"g":     10**0 
+			}
+    U = convert_sequence
+    return result*U[units]/U[new_units], new_units
 
 def check_GCTL_exceedance(parameter, result, units, matrix, qualifier):
     if matrix != 'W':
