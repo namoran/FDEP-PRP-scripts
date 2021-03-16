c6a3cb2c35d79d1213889419dfd9abaa861f38c1
nicholas <namoran@gmail.com>
2021-03-16 02:22:38 -0400

    updated EDD_functions to make convert units useful

--- EDD_functions.py
+++ EDD_functions.py
@@ -8,16 +8,23 @@ def check_detection(qualifier):
 def convert_units(result, units, new_units):
     '''convert units of the results fed into the function'''
     #the units tyoes are limited to ug/l ug/kg mg/kg mg/l ppm ppb
-    convert_sequence = {"ug/l":	 10**(-6),
-			"ug/kg": 10**(-6),
+    convert_sequence = {"u":	 10**(-6),
 			"ppb":	 10**(-6),
-			"mg/l":  10**(-3),
-			"mg/kg": 10**(-3),
+			"m": 	 10**(-3),
 			"ppm":	 10**(-3),
-			"g":     10**0 
+			"":     10**0 
 			}
+    
     U = convert_sequence
-    return result*U[units]/U[new_units], new_units
+    if units in {"ppb", "ppm"} and new_units in {"ppb", "ppm"}:
+        return result*U[units]/U[new_units], new_units
+    if units in {"ppb", "ppm"}:
+        return result*U[units]/U[new_units[0]], new_units
+    if new_units in {"ppb", "ppm"}:
+        return result*U[units[0]]/U[new_units], new_units
+    if units[-1] == new_units[-1]:
+        return result*U[units[0]]/U[new_units[0]], new_units
+    
 
 def check_GCTL_exceedance(parameter, result, units, matrix, qualifier):
     if matrix != 'W':
